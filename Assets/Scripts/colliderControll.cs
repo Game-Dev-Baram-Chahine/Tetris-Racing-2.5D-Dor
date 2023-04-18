5534f21b14871a6569dd98653173dda4cc17079a
DorBaram <73188591+DorBaram@users.noreply.github.com>
2023-04-18 22:34:30 +0300

    Update colliderControll.cs

--- Assets/Scripts/colliderControll.cs
+++ Assets/Scripts/colliderControll.cs
@@ -6,9 +6,12 @@ using UnityEngine.SceneManagement;
 public class colliderControll : MonoBehaviour
 {
     void OnTriggerEnter(Collider collider)
-    if(collider.tag == "Car")
+    {
+        if(collider.tag == "Car")
         {
             int index = SceneManager.GetActiveScene().buildIndex + 1;
             SceneManager.LoadScene(index);
         }
+        
+    }
 }
